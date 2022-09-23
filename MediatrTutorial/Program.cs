eda3fdea5ee194de7f85cbcf0e8188e3c35b1c19
Rambod Mogahdam <rambod.savahelimoghadam@webjar.ir>
2022-09-23 17:26:31 +0330

    Add GlobalUsing.cs

--- MediatrTutorial/Program.cs
+++ MediatrTutorial/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
